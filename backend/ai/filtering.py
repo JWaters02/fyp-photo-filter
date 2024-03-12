c2240a808f102581f4e10bddd9263d51d997b868
JWaters02 <watersjoshua2002@gmail.com>
2024-03-12 01:42:37 +0000

    rule filter system

--- backend/ai/filtering.py
+++ backend/ai/filtering.py
@@ -1,8 +1,52 @@
+import time
+import shutil
+import os
 from firebase import *
 from ai.image_cutout import *
 from ai.recognition import run_deepface
 from ai.recognition_facenet import run_facenet
-import time
+
+def apply_rules_and_sort_images(all_matches, rules, users, original_names):
+    visible_images_for_user = {uid: [] for uid in users}
+    visible_for_noone = []
+
+    for face_filename, portrait_filename in all_matches.items():
+        uploader_uid = face_filename.split('/')[1]
+        subject_uid = portrait_filename.split('/')[1]
+
+        # Default: image is visible to everyone except the uploader
+        excluded_users = set()
+
+        # Apply 'hideAllPhotosUploadedByMeFrom' rule
+        uploader_rules = rules.get(uploader_uid, {})
+        hide_all_from = uploader_rules.get('hideAllPhotosUploadedByMeFrom', {})
+        excluded_users.update(hide_all_from.keys())
+
+        # Apply 'hideMyPhotosContainingMeFrom' rule for the subject in the photo
+        subject_rules = rules.get(subject_uid, {})
+        hide_photos_containing_me_from = subject_rules.get('hideMyPhotosContainingMeFrom', {})
+        excluded_users.update(hide_photos_containing_me_from.keys())
+
+        # Determine the original image name from the face cutout path
+        original_image_name = original_names[face_filename]
+
+        # If the image is not visible to anyone, add it to the list of images that are not visible to anyone and continue to the next image
+        if len(excluded_users) == len(users):
+            visible_for_noone.append(original_image_name)
+            continue
+
+        # Add the image to the visible list for each user, excluding those specified by the rules
+        for uid in users:
+            if uid not in excluded_users:
+                new_path = f'photos/{uid}/sorted/{original_image_name}'
+                visible_images_for_user[uid].append(new_path)
+                if not os.path.exists(new_path):
+                    print(new_path)
+                    # os.makedirs(new_path)
+                # shutil.copy(face_filename, new_path)
+
+    print(visible_images_for_user, visible_for_noone)
+    return visible_images_for_user, visible_for_noone
 
 def filter_images(familyName: str):
     users = get_uids(familyName)
@@ -74,4 +118,36 @@ def filter_images(familyName: str):
     print(unsorted_images)
     print(all_matches)
     print(deepface_success_counter)
+
+    # now we have the matches, we can start to apply the rules to the images
+    # the rules are used to determine who can see what images
+    # a match is a dictionary of the form {face_filename: portrait_filename}
+    # face_filename is the path to the face image, not the original image itself. however the original image itself's name is in the path
+    # example:
+    # {'photos/PnKqc9B3hjV5cxmhYztxCzARkdJ3/cutouts/IMG-20240224-WA0011/IMG-20240224-WA0011_1.jpg': 'photos/VYmXtICASbPnaNTAHswCeb9hk692/portrait/portrait.jpg', 'photos/PnKqc9B3hjV5cxmhYztxCzARkdJ3/cutouts/IMG-20240224-WA0011/IMG-20240224-WA0011_2.jpg': 'photos/PnKqc9B3hjV5cxmhYztxCzARkdJ3/portrait/portrait.jpg'}
+    # example rules format:
+    # rules: {
+    # 'PnKqc9B3hjV5cxmhYztxCzARkdJ3': {'hideAllPhotosUploadedByMeFrom': {'yGIFu4hIFLUtGSTl3yHyxdC8inC2': 'Paul Waters'}, 'hideMyPhotosContainingMeFrom': {'VrDESpVst4Vw5NBfCWDnac053ze2': 'Ben Waters'}}
+    # 'VYmXtICASbPnaNTAHswCeb9hk692': {'hideMyPhotosContainingMeFrom': {'cPVXNlJbTCT0VkcgXrCz0bvQQHC3': 'Bobby Waters'}}
+    # }
+    # remember we have the users dictionary which maps the uid to their name
+    # so we can use this to figure out who is who in the rules
+    # hideAllPhotosUploadedByMeFrom means that the user wants to hide all photos they uploaded from another user
+    # hideMyPhotosContainingMeFrom means that the user wants to hide all photos containing them from another user
+    # the rules are applied in the order they are listed in the rules dictionary
+    # so if a user has both rules, the first rule will be applied first
+    # once the rule has been applied, we know which users to NOT show the image to, so then we know all the uids that we can show the image to
+    # so the path for the image can be moved to the appropriate folder for each user that can see the image
+    # so let's say we're sorting the image photos/PnKqc9B3hjV5cxmhYztxCzARkdJ3/cutouts/IMG-20240224-WA0011.jpg (the original image from the two cutout matches from example above)
+    # PnKqc9B3hjV5cxmhYztxCzARkdJ3 uploaded the image and they have the rule hideAllPhotosUploadedByMeFrom: yGIFu4hIFLUtGSTl3yHyxdC8inC2
+    # so we know that we can't show the image to yGIFu4hIFLUtGSTl3yHyxdC8inC2
+    # so we can show the image to all the other users
+    # so now construct the list of new output paths, one for each user. the path will be photos/{uid}/sorted/{original_image_name}
+    # then move the image to the new path
+    visible_images, non_visible_images = apply_rules_and_sort_images(all_matches, rules, users, original_names)
+    unsorted_images.extend(non_visible_images)
+
+    # now we have the images sorted into the appropriate folders, we can upload them to firebase
+    
+
     return None
